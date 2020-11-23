b6becd313e94d1ce103e739cb9c857290fa9183f
iago00 <Iago-2001>
2020-11-23 13:02:08 +0100

    resubido

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -1,18 +1,3 @@
-/*
- * Copyright 2018, The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package com.example.android.diceroller
 
@@ -23,32 +8,33 @@ import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-
+    //definimos ImageView
     lateinit var diceImage: ImageView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        //definimos el boton tirar dado
         val rollButton: Button = findViewById(R.id.roll_button)
+        //llama a la función rollDice cuando se hace click en el boton de tirar
         rollButton.setOnClickListener {
             rollDice()
         }
 
-        diceImage = findViewById(R.id.dice_image)
+        diceImage = findViewById(R.id.dicce_image)//se carga el ImageView
+        diceImage.setImageResource(R.drawable.dado)//se muestra una imagen dentro del ImageView
     }
 
     private fun rollDice() {
+        //generamos un numero aleatorio con la función random
         val randomInt = Random().nextInt(6) + 1
+        //dependiendo del numero que salga de la función random se muestra una imagen diferente
         val drawableResource = when (randomInt) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
+            1 -> diceImage.setImageResource(R.drawable._1)
+            2 -> diceImage.setImageResource(R.drawable._2)
+            3 -> diceImage.setImageResource(R.drawable._3)
+            4 -> diceImage.setImageResource(R.drawable._4)
+            5 -> diceImage.setImageResource(R.drawable._5)
+            else -> diceImage.setImageResource(R.drawable._6)
         }
-
-        diceImage.setImageResource(drawableResource)
     }
 }
